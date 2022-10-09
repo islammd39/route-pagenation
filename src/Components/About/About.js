7ccd173fd049a88f6a639b554e20d0314fd2680d
islammd39e <islammdrahat308@gmail.comm>
2022-10-09 19:32:30 +0600

    link add via component

---
+++ src/Components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const About = () => {
+    return (
+        <div>
+
+            <Header></Header>
+            <h1>This is About section</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Components/Service/Service.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Service = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>This is Service section</h1>
+        </div>
+    );
+};
+
+export default Service;

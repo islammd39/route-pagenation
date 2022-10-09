c301a5d51474aace1dcf46a1aab438b8b0b84816
islammd39e <islammdrahat308@gmail.comm>
2022-10-09 23:03:09 +0600

    user components style

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Post.css'
 
 const Post = ({post}) => {
-    const {userId,title}=post
+    const {userId,title,id}=post
     // console.log(post);
     return (
          <div className='post-id'>
+            <p>Post Id : <Link to={`/users/${id}`} >{id}</Link></p>
             <p>User Id : {userId}</p>
             <h3>Title : {title}</h3>
         </div>
